01705015e256b08331f95919c5360e1f68d685a4
ypark <miraclehand@naver.com>
2024-10-28 22:54:52 +0900

    Updated docker script to utilize caching

--- app/business-white-paper/build_docker_image.sh
+++ app/business-white-paper/build_docker_image.sh
@@ -1,4 +1,4 @@
-docker build --no-cache -t localhost:32000/business-white-paper:latest .
+docker build -t localhost:32000/business-white-paper:latest .
 docker push localhost:32000/business-white-paper:latest
 microk8s kubectl rollout restart deployment business-white-paper
 
