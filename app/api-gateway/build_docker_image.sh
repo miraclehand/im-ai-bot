7d6b047c3e147e04b189e0a1b73c38ea2bab79cc
ypark <miraclehand@naver.com>
2024-10-29 11:24:46 +0900

    Updated docker script to utilize caching

--- app/api-gateway/build_docker_image.sh
+++ app/api-gateway/build_docker_image.sh
@@ -1,4 +1,13 @@
-docker build -t localhost:32000/api-gateway:latest .
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    PLATFORM="amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+    PLATFORM="arm64"
+else
+    PLATFORM=$ARCH
+fi
+
+docker build --build-arg PLATFORM=$PLATFORM -t localhost:32000/api-gateway:latest .
 docker push localhost:32000/api-gateway:latest
 microk8s kubectl rollout restart deployment api-gateway
 
--- app/business-white-paper/build_docker_image.sh
+++ app/business-white-paper/build_docker_image.sh
@@ -1,4 +1,13 @@
-docker build -t localhost:32000/business-white-paper:latest .
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    PLATFORM="amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+    PLATFORM="arm64"
+else
+    PLATFORM=$ARCH
+fi
+
+docker build --build-arg PLATFORM=$PLATFORM -t localhost:32000/business-white-paper:latest .
 docker push localhost:32000/business-white-paper:latest
 microk8s kubectl rollout restart deployment business-white-paper
 
